d9d9a2138b9eb44b9326f7e3d9cb52ae15ae3519
merinmagi05 <144668865+merinmagi05@users.noreply.github.com>
2024-08-21 15:23:22 +0530

    Add files via upload

---
+++ cycle3/prgm9.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+iris=pd.read_csv("iris.csv")
+sns.displot(iris['sepal.length'],kde=True,rug=True)
+plt.title("Distribution of Sepal length")
+plt.show()
+
+sns.histplot(iris['petal.width'],kde=True,bins=20)
+plt.title("Histogram of petal width")
+plt.show()
+
+sns.relplot(x="sepal.length",y="sepal.width",data=iris,hue="variety",style="variety")
+plt.title("Sepal Length v/s Sepal Width")
+plt.show()
