e90131e34b53c3c36db31d9ded1c71dc866144df
merinmagi05 <144668865+merinmagi05@users.noreply.github.com>
2024-09-04 10:38:25 +0530

    Add files via upload

---
+++ cycle4/prgm2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from cycle4.prgm1 import classifier
+
+dataset=pd.read_csv("Blood Transfusion.csv")
+x=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,4].values
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.20)
+from sklearn.neighbors import KNeighborsClassifier
+classifier=KNeighborsClassifier(n_neighbors=5)
+classifier.fit(x_train,y_train)
+y_pred=classifier.predict(x_test)
+from sklearn.metrics import classification_report,confusion_matrix
+print(classification_report,confusion_matrix)
